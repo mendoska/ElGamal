1470e0c2eba54e8847684dadcba8b716a6d60c43
alex mendoza <alexandermen2017@gmail.com>
2023-03-28 17:08:31 -0400

    created way to get binary with desired bit size

--- main.cpp
+++ main.cpp
@@ -22,11 +22,34 @@ string decimalToBinary(int dec){
         binary =(dec % 2 == 0 ? "0":"1") +binary;
         dec = dec/2;
     }
-//    while(binary.length()<4){
-//        binary = "0" + binary;
-//    }
     return binary;
 }
+
+
+//makes binary string based on number of bits
+string intToBinary(int n, int numBits) {
+    string binaryStr;
+    for (int i = numBits - 1; i >= 0; i--) {
+        //<< left bit shift 1<<i, shifts binary representation of 1 to the left by i positions
+        binaryStr += ((n & (1 << i)) ? '1' : '0');
+    }
+    return binaryStr;
+}
+
+int binaryToDecimal(string binary){
+    int decimal = 0;
+    int counter = 0;
+    int size = binary.length();
+
+    for (int i = size-1; i>=0; i--){
+        if (binary[i] == '1'){
+            decimal += pow(2,counter);
+        }
+        counter++;
+    }
+    return decimal;
+}
+
 
 //needed for indexing in algorithm for SSM
 void reverseStr(string& str)
@@ -94,7 +117,7 @@ int SSM(int a, int k, int n){
 }
 
 
-//issue with  79
+//issue with  #79 and a few other numbers
 //Miller_Rabin Probabilistic Primality Test
 string millerRabin(int n, int t){
     string composite = "composite";
@@ -118,7 +141,7 @@ string millerRabin(int n, int t){
    //if n is divisible by 2
    //increment s
     if (nMinus1%2 == 0){
-        nMinus1 = nMinus1/2;
+        nMinus1 = (nMinus1)/2;
         s++;
     }
     //when is n no longer divisible by 2 that means it will be r
@@ -159,7 +182,7 @@ string millerRabin(int n, int t){
 
 
 //implementation of elGamal
-void elGamal (){
+void elGamal (int bits, string plainText){
 
 
    //PRIVATE KEY
@@ -177,6 +200,9 @@ void elGamal (){
     p = randNumGen(0,1000);
     test = millerRabin(p,4);
     }
+
+
+
 
    //for now i will be providing a generator variable of 2
    //link either crypto ++ or hardcoded function to find generator alpha
@@ -197,15 +223,18 @@ void elGamal (){
     //public keys
     cout<<"Your public keys are as follows: " << "p: "<< p << "alpha: " <<alpha
     << "alpha ^a mod p:" <<alphaPowA<<endl;
-    cout<< "Private key (a): "<< a;
+
+    cout<< "Private key (a): "<< a<<endl;
 
     //1b
     //represent message "m" as an integer
 
-    //for implementation v.1 I will set a value for message
-    //later i will create method to get integer value
+    //create an m variable that will represent the message in an integer in the range of {0,p-1}
 
-    int m =235;
+    int m = randNumGen(0, p-1);
+    cout<< "Your plaintext " <<plainText<<" , will be represented by this m = "<<m<<endl;
+
+
 
     //1c
     //select random integer
@@ -243,7 +272,7 @@ void elGamal (){
     int decMessage = (gammaPMinus1A *delta)%p;
 
     cout<<"Your original message"<<m<<endl;
-    cout<<"Decrypted message (original message)"<<decMessage;
+    cout<<"Decrypted message (original message)"<<decMessage<<endl;
 
 }
 
@@ -252,54 +281,33 @@ void elGamal (){
 
 int main()
 {
-//    int keySize = 0;
-//    string plainText = "";
+    int keySize = 0;
+    string plainText = "";
 
-//    //plaintxt
-//    cout << "Please enter your plaintext message";
-//    getline(cin,plainText);
+    //bits
+    cout<<"Please enter your keysize, this will determine how large your prime # will be: ";
+    cin >>keySize;
 
-//    //bits
-//    cout<<"Please enter your keysize by number of bits you would like to use \n";
-//    cin >>keySize;
+    cout << "\nPlease enter your plaintext message: ";
+    getline(cin,plainText);
 
+    //converting string to int
+   // int plainTextInt = stoi(plainText);
 
-    string str = millerRabin(7919,4);
-   cout << str<<endl;
+    elGamal(keySize,plainText);
+
 
 
     return 0;
 }
 
 
+//    string str = millerRabin(7919,4);
+//   cout << str<<endl;
 
-////turns decimal to binary
-//std::string decimalToBinary(int dec){
-//    std::string binary;
 
-//    while (dec!=0 ){
-//        binary =(dec % 2 == 0 ? "0":"1") +binary;
-//        dec = dec/2;
-//    }
-//    while(binary.length()<4){
-//        binary = "0" + binary;
-//    }
-//    return binary;
-//}
 
-//int binaryToDecimal(std::string binary){
-//    int decimal = 0;
-//    int counter = 0;
-//    int size = binary.length();
 
-//    for (int i = size-1; i>=0; i--){
-//        if (binary[i] == '1'){
-//            decimal += pow(2,counter);
-//        }
-//        counter++;
-//    }
-//    return decimal;
-//}
 
 
 
